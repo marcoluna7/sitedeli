70e4d35962a4e1e2d1f371018568cc13442969ed
marcoluna7 <markorex7@gmail.com>
2013-07-09 09:32:59 -0500

    Ultimos cambios

--- AlldeliWebSite/LogicaNegocios/Models/ModelsEntidades.designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'c:\users\w530\documents\visual studio 2012\Projects\EntidadesAllDely\EntidadesAllDely\ModelsEntidades.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
